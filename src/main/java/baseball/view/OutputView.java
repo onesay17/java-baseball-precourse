6013753f613c19b4a14dc62aa8711a61b9a7d973
onesay17 <onesay17@gmail.com>
2024-11-01 11:13:22 +0900

    feat: 출력값을 관리하는 뷰 클래스 생성

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static void printResult(String result) {
+        System.out.println(result);
+    }
+
+    public static void printGameOver() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}

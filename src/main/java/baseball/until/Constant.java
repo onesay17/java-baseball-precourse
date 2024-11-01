b29f8f881e60b0e8844e00337a58db853ec10e32
onesay17 <onesay17@gmail.com>
2024-11-01 11:12:53 +0900

    feat: 상수를 관리하는 클래스 생성

---
+++ src/main/java/baseball/until/Constant.java
@@ -0,0 +1,12 @@
+package baseball.until;
+
+public class Constant {
+
+    public static final Integer MIN_RANDOM_VALUE = 1;
+    public static final Integer MAX_RANDOM_VALUE = 9;
+    public static final Integer RANDOM_NUMBERS_SIZE = 3;
+    public static final String NOTHING = "낫싱";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final Integer WINNING_STRIKE_COUNT = 3;
+}
